1e57683e88ffe5ab53999e31548f53c8d8d0b8b9
jaewanpark <jaewan1040@yonsei.ac.kr>
2024-07-19 16:25:47 +0900

    07192024

--- dj4e-samples/bookone/apps.py
+++ dj4e-samples/bookone/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
 
-
 class BookoneConfig(AppConfig):
     name = 'bookone'
--- dj4e-samples/bookone/models.py
+++ dj4e-samples/bookone/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 
 class Lang(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200) #simple model
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
     isbn = models.CharField(max_length=13)
-    lang = models.ForeignKey('Lang', on_delete=models.SET_NULL, null=True)
+    lang = models.ForeignKey('Lang', on_delete=models.SET_NULL, null=True) #this starts the relational database
+    #deleted table is set to null
+    #since books can have no language
 
 class Instance(models.Model):
-    book = models.ForeignKey('Book', on_delete=models.CASCADE)
+    book = models.ForeignKey('Book', on_delete=models.CASCADE) #this is the second part of the relational database
+    #this will delete the whole row if the relation is deleted
     due_back = models.DateField(null=True, blank=True)
 
+#the migrations will link the models to the database
