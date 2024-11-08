aa10f689d17fd7b2a96066ed569aeca92c4ad2f5
jaewanpark <jaewan1040@yonsei.ac.kr>
2024-11-08 14:10:27 +0900

    11082024

--- dj4e-samples/menu/urls.py
+++ dj4e-samples/menu/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 app_name='menu'
+#TemplateView is sufficient for simple menus
+#
 urlpatterns = [
     path('', TemplateView.as_view(template_name='menu/main_menu.html'), name='main'),
     path('page1', TemplateView.as_view(template_name='menu/main_menu.html'), name='page1'),
